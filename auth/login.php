9bdb463003a84e899f8a817403ce09e4c74a92ed
Fearkunn <richiedaryl@gmail.com>
2024-12-04 11:20:23 +0700

    modify db connection and login

--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+include ('C:\xampp\htdocs\project tekweb\project-tekweb-1\db_connect\DatabaseConnection.php');
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +13,14 @@
   <title>login_page</title>
 </head>
 <body>
-  
+  <h2>Login: </h2>
+  <form action="login.php" method="post">
+    <label for="username">Username: </label>
+    <input type="text" name="username" required><br><br>
+
+    <label for="password">Password: </label>
+    <input type="password" name="password" required><br><br>
+    <button class="submit">Submit</button>
+  </form>
 </body>
 </html>
--- db_connect/DatabaseConnection.php
+++ db_connect/DatabaseConnection.php
@@ -2,14 +2,16 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = ""; //isi nama db kalian sendiri
+$dbname = "project"; //isi nama db kalian sendiri
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
 // Close the connection
 $conn->close();
 ?>
