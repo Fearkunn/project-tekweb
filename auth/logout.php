61b377cb2c66c58dd94a80cd3ced5815bd936606
Ivan-Ndaktau <c14230064@john.petra.ac.id>
2024-12-04 23:27:19 +0700

    fixed: cookie

--- auth/logout.php
+++ auth/logout.php
@@ -1,10 +1,14 @@
 <?php
+// Proses logout
 session_start();
-session_unset();  // Menghapus semua session
-session_destroy();  // Menghancurkan session
-setcookie('remember_token', '', time() - 3600, "/");
+session_unset();
+session_destroy();
+
+// Hapus cookie 'remember_me_token' setelah logout
+setcookie('remember_me_token', '', time() - 3600, '/', '', false, true);
 
 // Redirect ke halaman login setelah logout
-header("Location: ../auth/login.php");
+header("Location: login.php");
 exit();
+
 ?>
