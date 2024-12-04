8b0195ff25e95a0090dc9c36b76d32345590cd6a
Fearkunn <richiedaryl@gmail.com>
2024-12-04 20:31:33 +0700

    fix error register and login

--- auth/login.php
+++ auth/login.php
@@ -1,6 +1,34 @@
 <?php
 session_start();
 include('../db_connect/DatabaseConnection.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $username = mysqli_real_escape_string($conn, $_POST['username']); //mysqli untuk mencegah sql injection
+    $password = mysqli_real_escape_string($conn, $_POST['user_password']);
+    
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
+    // Ambil data hasil query
+    $user = mysqli_fetch_assoc($result);
+
+    // Validasi apakah user ditemukan
+    if ($user) {
+        // Verifikasi password
+        if (password_verify($password, $user['user_password'])) {
+            $_SESSION['username'] = $user['username'];
+
+            // Redirect ke halaman utama
+            header("Location: ../main_form/mainForm.php");
+            exit();
+        } else {  
+            $error_message = "Password salah.";
+        }
+    } else {
+        $error_message = "Username tidak ditemukan.";
+    }
+}
 ?>
 
 <!doctype html>
@@ -200,33 +228,3 @@ include('../db_connect/DatabaseConnection.php');
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
-<?php
-   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
-    $username = mysqli_real_escape_string($conn, $_POST['username']); //mysqli untuk mencegah sql injection
-    $password = mysqli_real_escape_string($conn, $_POST['user_password']);
-    
-
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $result = mysqli_query($conn, $query);
-    // Ambil data hasil query
-    $user = mysqli_fetch_assoc($result);
-
-    // Validasi apakah user ditemukan
-    if ($user) {
-        // Verifikasi password
-        if (password_verify($password, $user['user_password'])) {
-            $_SESSION['username'] = $user['username'];
-
-            // Redirect ke halaman utama
-            header("Location: ../main_form/mainForm.php");
-            exit();
-        } else {  
-            $error_message = "Password salah.";
-        }
-    } else {
-        $error_message = "Username tidak ditemukan.";
-    }
-}
-?>
