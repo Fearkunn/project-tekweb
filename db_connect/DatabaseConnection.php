0ffeeac5be8faf684e2abdf77238391181e92e59
Fearkunn <richiedaryl@gmail.com>
2024-12-04 10:58:27 +0700

    first commit

---
+++ db_connect/DatabaseConnection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = ""; //isi nama db kalian sendiri
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+// Close the connection
+$conn->close();
+?>
