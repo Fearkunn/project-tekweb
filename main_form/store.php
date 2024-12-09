e3560854307979213aa8b042d40410df64c553c4
Fearkunn <richiedaryl@gmail.com>
2024-12-09 14:18:13 +0700

    back to main form

--- main_form/store.php
+++ main_form/store.php
@@ -73,10 +73,12 @@ $result = $stmt->get_result();
     </style>
 </head>
 <body>
-    <div class="store-header">
-        <h1>Game Store</h1>
-        <p>Temukan game favoritmu!</p>
-    </div>
+<div class="store-header">
+    <a href="../main_form/mainForm.php" class="text-decoration-none text-dark">
+        <h1 style="color:white;">Game Store</h1>
+    </a>
+    <p>Temukan game favoritmu!</p>
+</div>
 
     <!-- Search Bar -->
     <div class="search-bar text-center">
